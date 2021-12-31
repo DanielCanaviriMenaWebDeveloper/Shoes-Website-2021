23218da611b76d6305ea2127dc3bce1e100bb7fa
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-12-30 23:23:46 -0400

    Header modo escritorio Finalizado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo");
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+    document.body.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    if(window.innerWidth < 1200) {
+        menu.classList.remove('fa-times');
+        header.classList.remove('active');
+        document.body.classList.remove('active');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+<h1>Web Site Demo : </h1>
+
+<p>https://danielcanavirimenawebdeveloper.github.io/Shoes-Website-2021/</p>
+
+<p>Desktop</p>
+<img src="images/imagen.png">
+
+<!-- <p>Tablets</p>
+<img src="images/imagen2.png">
+
+<p>Phones</p>
+<img src="images/imagen3.png"> -->
+
+<p>Page Under Development</p>
